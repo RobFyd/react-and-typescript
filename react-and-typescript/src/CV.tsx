9965fdcae7c52322bf48bad8a75b5a1d76593523
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-11-20 00:24:20 +0000

    fix: add optional children prop and improve formatting in CV component

--- react-and-typescript/src/CV.tsx
+++ react-and-typescript/src/CV.tsx
@@ -2,14 +2,15 @@ interface CVProps {
     name: string;
     currentRole: string;
     jobHistory: string[];
+    children?: React.ReactNode;
 }
 
 export function CV({ name, currentRole, jobHistory }: CVProps) {
     return (
         <>
             <h1>{name}</h1>
-            <h2>Current role:{currentRole}</h2>
-            <p>Job History</p>
+            <h2>Current role: {currentRole}</h2>
+            <p>Job History:</p>
             {jobHistory.map((job, index) => {
                 return <p key={index}>{job}</p>
             })}
